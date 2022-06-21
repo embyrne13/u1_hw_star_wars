35ce47d2d8d163bcf14077ca9c38e80a8a7caa3c
Emily Byrne <embyrne13@gmail.com>
2022-06-21 18:27:43 -0400

    StarWars Homework

--- act2.sh
+++ act2.sh
@@ -1,5 +1,5 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND using &&) create a file called princess_leia.txt with the text "Help me, Obi-Wan…"
-touch princess_leia.txt echo "Help me, Obi-Wan…"
+touch princess_leia.txt & echo "Help me, Obi-Wan…"
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
 touch obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
